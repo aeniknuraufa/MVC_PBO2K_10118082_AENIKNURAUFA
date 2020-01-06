5f5ef91681193da668018758c0a4e9023e6093c5
Aenik Nur Aufa <aeniknuraufa@users.noreply.github.com>
2020-01-06 22:38:24 +0700

    MVC_PBO2K_10118082_AENIKNURAUFA
    Latihan membuat MVC

---
+++ src/edu/aeniknuraufa/latihanmvc/event/PelangganListener.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.aeniknuraufa.latihanmvc.event;
+
+import edu.aeniknuraufa.latihanmvc.model.PelangganModel;
+
+/**
+ *
+ * @author Aenik Nur Aufa
+ */
+public interface PelangganListener {
+    
+    public void onChange(PelangganModel pelanggan);
+}
---
+++ src/edu/aeniknuraufa/latihanmvc/model/LatihanMVC.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.aeniknuraufa.latihanmvc.model;
+
+import edu.aeniknuraufa.latihanmvc.view.PelangganView;
+
+/**
+ *
+ * @author Aenik Nur Aufa
+ */
+public class LatihanMVC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        PelangganView pelangganView = new PelangganView();
+        pelangganView.setVisible(true);
+    }
+    
+}
